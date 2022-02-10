6126f20b04a343a9fb524aa32af8616fc86a866f
Guillaume GB <guillaume.gb@live.ca>
2022-02-10 09:59:49 -0500

    Ajout du material ui et d'un peu de css

--- src/BtnAjoutPanier.jsx
+++ src/BtnAjoutPanier.jsx
@@ -1,7 +1,10 @@
 import './BtnAjoutPanier.scss';
+import Badge from '@mui/material/Badge';
 
 export default function BtnAjoutPanier () {
     return(
-        <button>Ajouter au panier</button>
+        <Badge badgeContent={2} color="primary">
+            <button className="BtnAjoutPanier">Ajouter au panier</button>
+        </Badge>
     );
 }
